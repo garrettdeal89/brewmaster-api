f4f6f69936f38d5e818394c8cedb75b7beb78cd8
Garrett Deal <garrettdeal89@gmail.com>
2026-06-30 13:03:32 -0400

    Build: bootstrap Springboot Project

---
+++ src/main/java/com/gdeal/brewmaster/BrewmasterApplication.java
@@ -0,0 +1,13 @@
+package com.gdeal.brewmaster;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewmasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewmasterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdeal/brewmaster/BrewmasterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdeal.brewmaster;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrewmasterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
