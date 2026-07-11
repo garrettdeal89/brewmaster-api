56ed5f8bb8402f1d6c2053b7958a2ff67c317dee
Garrett Deal <garrettdeal89@gmail.com>
2026-07-10 22:17:02 -0400

    feat: migrate project to postgresql with docker profiles.

--- src/test/java/com/gdeal/brewmaster/BrewmasterApplicationTests.java
+++ src/test/java/com/gdeal/brewmaster/BrewmasterApplicationTests.java
@@ -2,12 +2,14 @@ package com.gdeal.brewmaster;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class BrewmasterApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
